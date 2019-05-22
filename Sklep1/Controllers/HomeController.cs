4576bd7bd69469258e45743cbba2dbbcb513e2f1
kateszy <szalskakatarzyna@gmail.com>
2019-05-22 23:01:02 +0200

    Poprawienie struktury gita oraz przerobienie strony na MVC

---
+++ Sklep1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sklep1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Sklep()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Koszyk()
+        {
+            return View();
+        }
+
+        public ActionResult Kontakt()
+        {
+            return View();
+
+        }
+       
+    }
+}
