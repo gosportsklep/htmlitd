6316ad45b8171d7a6cc76e3ea1fa9a0bd120726f
kateszy <szalskakatarzyna@gmail.com>
2019-08-14 21:33:05 +0200

    Added mail sending contact form

--- Sklep1/Controllers/HomeController.cs
+++ Sklep1/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity.Validation;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -81,45 +83,90 @@ namespace Sklep1.Controllers
             return View();
         }
 
+        ////Example of creating product check database model for more information
+        //Product p = new Product();
+        //p.Name = "stol";
+        //    p.Description = "Opis stolu 10 znakow";
+        //    p.Category = "pokoj";
+
+        //    Product p1 = new Product();
+        //p1.Name = "fotel";
+        //    p1.Description = "Opis fotela 10 znakow";
+        //    p1.Category = "pokoj";
+
+        //    Product p2 = new Product();
+        //p2.Name = "kuchenka";
+        //    p2.Description = "Opis kuchenki 10 znakow";
+        //    p2.Category = "kuchnia";
+
+        //    db.Products.Add(p);
+        //    db.Products.Add(p1);
+        //    db.Products.Add(p2);
+        //    try
+        //    {
+        //        db.SaveChanges();
+
+        //    }
+        //    catch (DbEntityValidationException ex)
+        //    {
+        //        foreach (var entityValidationErrors in ex.EntityValidationErrors)
+        //        {
+        //            foreach (var validationError in entityValidationErrors.ValidationErrors)
+        //            {
+        //                Response.Write("Property: " + validationError.PropertyName + " Error: " + validationError.ErrorMessage);
+        //            }
+        //        }
+        //        ViewBag.Message = "Something wrong";
+        //    }
+        //    return View();
+
         public ActionResult Kontakt()
         {
-            //Example of creating product check database model for more information
-            Product p = new Product();
-            p.Name = "stol";
-            p.Description = "Opis stolu 10 znakow";
-            p.Category = "pokoj";
-
-            Product p1 = new Product();
-            p1.Name = "fotel";
-            p1.Description = "Opis fotela 10 znakow";
-            p1.Category = "pokoj";
-
-            Product p2 = new Product();
-            p2.Name = "kuchenka";
-            p2.Description = "Opis kuchenki 10 znakow";
-            p2.Category = "kuchnia";
-
-            db.Products.Add(p);
-            db.Products.Add(p1);
-            db.Products.Add(p2);
-            try
-            {
-                db.SaveChanges();
-
-            }
-            catch (DbEntityValidationException ex)
-            {
-                foreach (var entityValidationErrors in ex.EntityValidationErrors)
-                {
-                    foreach (var validationError in entityValidationErrors.ValidationErrors)
-                    {
-                        Response.Write("Property: " + validationError.PropertyName + " Error: " + validationError.ErrorMessage);
-                    }
-                }
-                ViewBag.Message = "Something wrong";
-            }
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Kontakt(string sender, string subject, string message)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var senderEmail = new MailAddress(sender, "Sender");
+                    var receiverEmail = new MailAddress("gosportsklp@gmail.com", "Receiver");
+                    var password = "gosport123#$";
+                    var sub = subject;
+                    var body = message;
+                    var smtp = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(receiverEmail.Address, password)
+
+                    };
+
+                    using (var mess = new MailMessage(senderEmail, receiverEmail)
+                    {
+                        Subject = senderEmail +" "+ subject,
+                        Body = body
+                    })
+                    {
+                        smtp.Send(mess);
+                    }
+                }
+                return View();
+            }
+
+            catch (Exception e)
+            {
+                ViewBag.Error = e.GetBaseException().ToString();
+               // ViewBag.Error = "Nie udało się wysłać, spróbuj ponownie lub póżniej";
+            }
+            return View();
+        }
+       
     }
 }
