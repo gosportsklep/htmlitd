2aa03b97521d47615b9f73402862cc4dfdb9df33
Unknown <szalskakatarzyna@gmail.com>
2019-09-20 02:52:53 +0200

    Update Koszyk

---
+++ Sklep1/Migrations/201909200050438_UserDatas.cs
@@ -0,0 +1,16 @@
+namespace Sklep1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UserDatas : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
